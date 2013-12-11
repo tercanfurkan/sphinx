ac8df065c0045afa5c80f2145c013ef740614ff8
tercanfurkan <tercanfurkan@gmail.com>
2013-12-11 23:19:23 +0200

    add MeterLimitVal class and remove 2 conf file from repo

---
+++ app/models/form/MeterLimitVal.java
@@ -0,0 +1,12 @@
+package models.form;
+
+public class MeterLimitVal {
+	
+	public MeterLimitVal() {
+		this.diameterMin = 333;
+		this.diameterMax = 666;
+	}
+
+	int diameterMin;
+	int diameterMax;
+}
